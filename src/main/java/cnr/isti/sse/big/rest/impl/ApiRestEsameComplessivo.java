c99fb473d51b22b964f01af267b6a2563434c96a
Giorgio <imatesiu@gmail.com>
2020-11-19 00:42:47 +0100

    interface esame complessivo added

---
+++ src/main/java/cnr/isti/sse/big/rest/impl/ApiRestEsameComplessivo.java
@@ -0,0 +1,212 @@
+package cnr.isti.sse.big.rest.impl;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import org.apache.commons.io.IOUtils;
+import org.glassfish.jersey.media.multipart.FormDataBodyPart;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+
+import cnr.isti.sse.big.data.reply.accessi.RiepilogoControlloAccessi;
+import cnr.isti.sse.big.data.reply.accessi.lta.LTAGiornaliera;
+import cnr.isti.sse.big.data.riepilogogiornaliero.RiepilogoGiornaliero;
+import cnr.isti.sse.big.data.riepilogomensile.RiepilogoMensile;
+import cnr.isti.sse.big.data.transazioni.LogTransazione;
+import cnr.isti.sse.big.util.Utility;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+
+
+@OpenAPIDefinition(info = @Info(title = "APID Service", version = "0.1"), servers = {@Server(url="/v1/dispositivi")
+})
+//@Consumes(MediaType.MULTIPART_FORM_DATA)
+// @Produces(MediaType.APPLICATION_XML)
+@Path("/biglietterie")
+public class ApiRestEsameComplessivo {
+
+	private static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(ApiRestEsameComplessivo.class);
+
+	
+
+	@Path("/EsameComplessivo")
+	@POST
+	@ApiResponse(
+			responseCode = "200",
+			content = @Content(
+					mediaType = MediaType.APPLICATION_XML,
+					schema = @Schema(implementation = String.class)
+					),
+			description = "."
+			)
+	@RequestBody(content = @Content(
+			mediaType = MediaType.MULTIPART_FORM_DATA//,
+			//	schema = @Schema(implementation = List<byte[]>.class)
+			),
+	description = "." )
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String putLogTransazioneFiles( @FormDataParam("files") List<FormDataBodyPart> uploadedInputStream,
+			@FormDataParam("files") List<FormDataContentDisposition> fileDetail, @Context HttpServletRequest request, @Context HttpServletResponse response)
+					throws JAXBException {// DatiCorrispettiviType Corrispettivi,
+		// @Context HttpServletRequest request){
+		response.setHeader("Connection", "Close");
+		log.info("****************List***LTA********************");
+		try{
+
+			log.info("Message from: "+request.getRemoteAddr());
+
+			
+
+			int i = 0;
+			for(FormDataBodyPart filePart: uploadedInputStream) {
+				byte[] your_primitive_bytes = IOUtils.toByteArray(filePart.getEntityAs(InputStream.class));
+
+				boolean	resul = Utility.verifyPKCS7(your_primitive_bytes);
+				log.info(resul);
+				byte[] t = 	Utility.getData(your_primitive_bytes);
+				String Transazione = new String(t);
+				String LT = Transazione.replaceAll("<!DOCTYPE.*", "").replaceAll("[^\\x20-\\x7e]", "");
+				String NameFile = fileDetail.get(i).getFileName().substring(0,3);
+				switch (NameFile) {
+				case "LTA":{
+					JAXBContext jaxbContext = JAXBContext.newInstance(LTAGiornaliera.class);
+					Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+					StringReader reader = new StringReader(LT);
+					LTAGiornaliera LogT = (LTAGiornaliera) unmarshaller.unmarshal(reader);
+					log.info("OK");
+					break;}
+				case "RCA":{
+					JAXBContext jaxbContext = JAXBContext.newInstance(RiepilogoControlloAccessi.class);
+					Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+
+					StringReader reader = new StringReader(LT);
+					RiepilogoControlloAccessi LogT = (RiepilogoControlloAccessi) unmarshaller.unmarshal(reader);
+					log.info("OK");
+					break;}
+				case "RPG":{
+					JAXBContext jaxbContext = JAXBContext.newInstance(RiepilogoGiornaliero.class);
+					Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+
+					StringReader reader = new StringReader(LT);
+					RiepilogoGiornaliero RPG = (RiepilogoGiornaliero) unmarshaller.unmarshal(reader);
+					log.info("OK");
+					break;}
+				case "RPM":{
+					JAXBContext jaxbContext = JAXBContext.newInstance(RiepilogoMensile.class);
+					Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+					
+					StringReader reader = new StringReader(LT);
+					RiepilogoMensile RPM = (RiepilogoMensile) unmarshaller.unmarshal(reader);
+					log.info("OK");
+
+					break;}
+				case "LOG":{
+					JAXBContext jaxbContext = JAXBContext.newInstance(LogTransazione.class);
+					Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+
+					StringReader reader = new StringReader(LT);
+					LogTransazione LogT = (LogTransazione) unmarshaller.unmarshal(reader);
+					log.info("OK");
+
+					break;}
+
+				default:
+					break;
+				}
+
+				i++;
+			}
+			/*{
+				byte[] t = 	Utility.getData(your_primitive_bytes);
+				String LogTransazione = new String(t);
+				String LT = LogTransazione.replaceAll("<!DOCTYPE LTA_Giornaliera SYSTEM.*", "").replaceAll("[^\\x20-\\x7e]", "");
+				JAXBContext jaxbContext = JAXBContext.newInstance(LTAGiornaliera.class);
+				Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+				Utility.validateXmlLogTransazione(unmarshaller);	
+
+				StringReader reader = new StringReader(LT);
+				LTAGiornaliera LogT = (LTAGiornaliera) unmarshaller.unmarshal(reader);
+
+				
+			}*/
+			//log.info("Logs: [NProgressivi, Corrispettivo Lordo, Prevendita Lordo] "+sum);
+			String text = "KO";
+			return text;//new Log(sum);
+			//throw new WebApplicationException(Response.status(406).entity(text).build());
+
+
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			log.error(e);
+		}
+		return null;
+		//return "";
+
+	}
+
+	@Path("/SingleFileLTA")
+	@POST
+	@ApiResponse(
+			responseCode = "200",
+			content = @Content(
+					mediaType = MediaType.APPLICATION_XML,
+					schema = @Schema(implementation = String.class)
+					),
+			description = "."
+			)
+	@RequestBody(content = @Content(
+			mediaType = MediaType.MULTIPART_FORM_DATA//,
+			//	schema = @Schema(implementation = List<byte[]>.class)
+			),
+	description = "." )
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	public String putLogTransazionel( @FormDataParam("file") InputStream uploadedInputStream,
+			@FormDataParam("file") FormDataContentDisposition fileDetail, @Context HttpServletRequest request, @Context HttpServletResponse response)
+					throws JAXBException {// DatiCorrispettiviType Corrispettivi,
+		// @Context HttpServletRequest request){
+		response.setHeader("Connection", "Close");
+		log.info("****************FileRiepilogoControlloAccessi********************");
+		try{
+
+			log.info("Message from: "+request.getRemoteAddr());
+
+
+			String text = "KO";
+			return text;
+			//throw new WebApplicationException(Response.status(406).entity(text).build());
+
+
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			log.error(e);
+		}
+		return "";
+
+	}
+
+
+
+	
+
+}
---
+++ src/test/java/isti/cnr/sse/rest/impl/API_EsameComplessivo_TEST.java
@@ -0,0 +1,342 @@
+package isti.cnr.sse.rest.impl;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringReader;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.security.Principal;
+import java.security.PublicKey;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.crypto.dsig.Reference;
+import javax.xml.crypto.dsig.XMLSignature;
+import javax.xml.crypto.dsig.XMLSignatureFactory;
+import javax.xml.crypto.dsig.dom.DOMValidateContext;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMResult;
+import javax.xml.transform.stream.StreamSource;
+
+import org.apache.commons.io.FileUtils;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataMultiPart;
+import org.glassfish.jersey.media.multipart.MultiPart;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.glassfish.jersey.media.multipart.file.FileDataBodyPart;
+import org.glassfish.jersey.message.internal.ReaderWriter;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletContainer;
+import org.glassfish.jersey.test.DeploymentContext;
+import org.glassfish.jersey.test.JerseyTest;
+import org.glassfish.jersey.test.ServletDeploymentContext;
+import org.glassfish.jersey.test.TestProperties;
+import org.glassfish.jersey.test.grizzly.GrizzlyWebTestContainerFactory;
+import org.glassfish.jersey.test.spi.TestContainerFactory;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+import cnr.isti.sse.big.data.transazioni.LogTransazione;
+import cnr.isti.sse.big.rest.impl.ApiRestEsameComplessivo;
+
+
+public class API_EsameComplessivo_TEST extends JerseyTest {
+
+	@Override
+	protected TestContainerFactory getTestContainerFactory() {
+		return new GrizzlyWebTestContainerFactory();
+	}
+
+	@Override
+	protected DeploymentContext configureDeployment() {
+		forceSet(TestProperties.CONTAINER_PORT, "0");
+		//register(MultiPartFeature.class);
+		return ServletDeploymentContext.forServlet(new ServletContainer(new ResourceConfig(MultiPartFeature.class).register(ApiRestEsameComplessivo.class)))
+				.build();
+
+	}
+	
+
+
+	/*@Override
+	protected Application configure() {
+		return new ResourceConfig(ApiRestLogAccessi.class);//.register(new MultiPartFeature());
+	}*/
+	
+	@Override
+    public void configureClient(ClientConfig config) {
+        config.register(MultiPartFeature.class).register(ApiRestEsameComplessivo.class);
+    }
+
+
+	@Test
+	public void test() throws ClassNotFoundException, IOException  {
+		
+		
+		
+		//String nameFilexml = "log.xml";//
+
+		
+	//runTest(nameFilexml);
+	
+
+	
+	String Filexml = "LTA_2020_11_18_001.xsi.p7m";
+	//runTest2(Filexml);
+	
+	List<String> files = new ArrayList<String>();
+	files.add(Filexml);
+	 Filexml = "LOG_2020_10_13_001.xsi.p7m";
+	files.add(Filexml);
+	Filexml = "RPG_2020_10_13_001.xsi.p7m";
+	files.add(Filexml);
+	 Filexml = "RPM_2020_10_00_001.xsi.p7m";
+	files.add(Filexml);
+	
+	runTest3(files);
+	/*File f = FileUtils.toFile( APIProveHWImplTest.class.getClassLoader().getResource(Filexml));
+	//InputStream content = new FileInputStream(f);
+	//final String read = ReaderWriter.readFromAsString(content, MediaType.APPLICATION_XML_TYPE);
+	
+	
+	
+	byte[] targetArray = fromFileToByteArray(f);
+	
+	byte[] t = 	Utility.getData(targetArray);
+	String string = new String(t);
+    //System.out.println(string);
+	Store<X509CertificateHolder> cert = Utility.getCert(targetArray);
+	
+	try {
+	 boolean	result = Utility.verifyPKCS7(targetArray,t);
+	 System.out.println(result);
+	} catch (CertificateException | StoreException | OperatorCreationException | NoSuchAlgorithmException
+			| NoSuchProviderException | InvalidNameException | CMSException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	System.out.println("");*/
+	}
+
+	
+	
+	private void runTest3(List<String> files) throws IOException, ClassNotFoundException {
+		MultiPart multipartEntity = new FormDataMultiPart();
+		for(String nameFilexml: files) {
+			File f = FileUtils.toFile( API_EsameComplessivo_TEST.class.getClassLoader().getResource(nameFilexml));
+			
+
+
+
+
+
+			FileDataBodyPart filePart = new FileDataBodyPart("files", 
+					f);
+
+			filePart.setContentDisposition(
+					FormDataContentDisposition.name("files")
+					.fileName(f.getName()).build());
+			
+				multipartEntity.bodyPart(filePart);
+			}
+			Entity<MultiPart> entity = Entity.entity(multipartEntity, multipartEntity.getMediaType());
+			//Entity<List<InputStream>> entity = Entity.entity(lbyte, MediaType.MULTIPART_FORM_DATA);
+			Response response = target("/biglietterie/EsameComplessivo/").request(MediaType.APPLICATION_JSON).post(entity);
+
+
+
+			String res2 = response.readEntity(new GenericType<String>() {
+			});
+
+
+			assertNotNull(response);
+		
+	}
+
+	private byte[] fromFileToByteArray(File file) {
+	    try {
+	        return FileUtils.readFileToByteArray(file);
+	    } catch (IOException e) {
+	    	System.err.println("Error while reading .p7m file!" + e);
+	    }
+	    return new byte[0];
+	}
+
+	
+	
+	
+	private void runTest2(String nameFilexml) throws JAXBException, IOException, URISyntaxException {
+		
+		
+
+
+		File f = FileUtils.toFile( API_EsameComplessivo_TEST.class.getClassLoader().getResource(nameFilexml));
+		InputStream content = new FileInputStream(f);
+		//final String read = ReaderWriter.readFromAsString(content, MediaType.APPLICATION_XML_TYPE);
+		byte[] targetArray = fromFileToByteArray(f);
+		
+		final Entity<byte[]> rex = Entity.entity(targetArray, MediaType.MULTIPART_FORM_DATA);
+
+
+		Response response = target("/biglietterie/LogTransazione/").request(MediaType.MULTIPART_FORM_DATA).post(rex);
+
+
+
+		String res2 = response.readEntity(new GenericType<String>() {
+		});
+		
+
+		assertNotNull(response);
+	}
+	
+	
+	private void runTest(String nameFilexml) throws JAXBException, IOException, URISyntaxException {
+		
+		
+		
+		InputStream is = API_EsameComplessivo_TEST.class.getClassLoader().getResourceAsStream(nameFilexml);
+		assertNotNull(is);
+		JAXBContext jaxbContexti = JAXBContext.newInstance(LogTransazione.class);
+
+		Unmarshaller jaxbUnmarshaller1 = jaxbContexti.createUnmarshaller();
+		LogTransazione collaborativeContentInput = (LogTransazione) jaxbUnmarshaller1.unmarshal(is);
+		//getMatricola(collaborativeContentInput);
+		Entity<LogTransazione> entity = Entity.entity(collaborativeContentInput, MediaType.APPLICATION_XML);
+
+
+		File f = FileUtils.toFile( API_EsameComplessivo_TEST.class.getClassLoader().getResource(nameFilexml));
+		InputStream content = new FileInputStream(f);
+		final String read = ReaderWriter.readFromAsString(content, MediaType.APPLICATION_XML_TYPE);
+
+		final Entity<String> rex = Entity.entity(read, MediaType.APPLICATION_XML_TYPE);
+
+
+		Response response = target("/biglietterie/LogTransazione/").request(MediaType.APPLICATION_XML).post(rex);
+
+
+
+		String res2 = response.readEntity(new GenericType<String>() {
+		});
+		
+
+		assertNotNull(response);
+	}
+
+	
+	private String stringtoStreaming(InputStream inputStream){
+
+		StringBuilder textBuilder = new StringBuilder();
+		try{ 
+			try (Reader reader = new BufferedReader(new InputStreamReader
+					(inputStream, Charset.forName(StandardCharsets.UTF_8.name())))) {
+				int c = 0;
+				while ((c = reader.read()) != -1) {
+					textBuilder.append((char) c);
+				}
+			}
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return textBuilder.toString();
+	}
+
+
+	private boolean statusCertificateAndSignature(byte[] certificate, String Document) {
+
+		//byte[] certificate = d.getSignature().getKeyInfo().getX509Data().getX509Certificate();
+		CertificateFactory fact = null;
+		try {
+			fact = CertificateFactory.getInstance("X.509");
+
+			X509Certificate cert = (X509Certificate) fact
+					.generateCertificate(new ByteArrayInputStream(certificate));
+
+			PublicKey publicKey = cert.getPublicKey();
+
+			Document doc = convertStringToDocument(Document);// marshallToDocument(d,DatiCorrispettiviType.class);
+
+			NodeList nl = doc.getElementsByTagNameNS(XMLSignature.XMLNS, "Signature");
+
+			if (nl.getLength() == 0) {
+				throw new Exception("Cannot find Signature element");
+			}
+
+			DOMValidateContext valContext = new DOMValidateContext(publicKey, nl.item(0));
+
+			XMLSignatureFactory fac = XMLSignatureFactory.getInstance("DOM");
+
+			XMLSignature signature = fac.unmarshalXMLSignature(valContext);
+
+			boolean validFlag = signature.validate(valContext);
+
+			// Check core validation status.
+			if (validFlag == false) {
+				
+				System.err.println("Signature failed core validation");
+				boolean sv = signature.getSignatureValue().validate(valContext);
+				System.out.println("signature validation status: " + sv);
+				if (sv == false) {
+					// Check the validation status of each Reference.
+					Iterator i = signature.getSignedInfo().getReferences().iterator();
+					for (int j = 0; i.hasNext(); j++) {
+						boolean refValid = ((Reference) i.next()).validate(valContext);
+						System.out.println("ref[" + j + "] validity status: " + refValid);
+					}
+				}
+			} else {
+				System.out.println("Signature passed core validation");
+			}
+
+			Principal principal = cert.getSubjectDN();
+			String name = principal.getName();
+			return validFlag;
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return false;
+
+	}
+
+
+	private static Document convertStringToDocument(String xmlStr) {
+		try {
+
+			DOMResult output = new DOMResult();
+			Transformer transformer = TransformerFactory.newInstance().newTransformer();
+			transformer.transform(new StreamSource(new StringReader(xmlStr)), output);
+
+			return (Document) output.getNode();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
