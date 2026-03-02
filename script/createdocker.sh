7925c4e08257e24334c744f973a0d9aa9d2ba417
Imatesiu <imatesiu@gmail.com>
2026-03-02 19:46:01 +0100

    Update createdocker.sh

--- script/createdocker.sh
+++ script/createdocker.sh
@@ -1,3 +1,3 @@
 docker build -t rpm-log-viewer .
-docker run --rm -p 8443:5000 -v "$(pwd)/tls:/app/tls:ro" -v "$(pwd)/uploads:/app/uploads" rpm-log-viewer
+docker run --rm -p 8443:5000 -v "$(pwd)/tls:/app/tls:ro" -v "$(pwd)/uploads:/app/uploads" --restart unless-stopped  rpm-log-viewer
 
