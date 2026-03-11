a7b1ed5ae69cb6ef640d493f2d28d3e5b936d653
Imatesiu <imatesiu@gmail.com>
2026-03-11 22:58:42 +0100

    Update createdocker.sh

--- script/createdocker.sh
+++ script/createdocker.sh
@@ -1,4 +1,5 @@
+docker rm -f rpm-log-viewer
 docker build -t rpm-log-viewer .
 #docker run -d --name rpm-log-viewer  -p 8443:5000 -v "$(pwd)/tls:/app/tls:ro" -v "$(pwd)/uploads:/app/uploads" --restart unless-stopped  rpm-log-viewer
-docker run -d --name rpm-log-viewer  -p 8443:5000 -v "$(pwd)/tls:/etc/letsencrypt/live/rtapp.isti.cnr.it:ro" -v "$(pwd)/uploads:/app/uploads" --restart unless-stopped  rpm-log-viewer
+docker run -d --name rpm-log-viewer -p 8443:5000 -v "$(pwd)/etc/letsencrypt/live/rtapp.isti.cnr.it:/app/tls:ro" -v "$(pwd)/uploads:/app/uploads" --restart unless-stopped  rpm-log-viewer
 
